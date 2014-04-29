e04f06783796e93281336dc06cd668804eb881ec
Yoon-Cho <yoon.cho@hotmail.com>
2014-04-29 08:44:10 -0400

    stuff

--- cho_yoon/16-Trees/BST.java
+++ cho_yoon/16-Trees/BST.java
@@ -6,13 +6,31 @@ public class BST{
 	root = null;
     }
 
+    /*
+1. get a pointer to the parent of the node we want to delete(one recursion?)
+2. 3 cases
+a.The node to delete is a leaf - set the parent's left or right pointer for appropriate to null
+b.The node to delete has one child - set the parent's left or right pointer(or appropriate) to the node's non-null pointer
+a and b can be contained
+c.The node to delete has two children - 
+- find the largest value on the left subtree 
+- make a copy of that value
+- pretend you have a tree and keep changing until you have a max of two children
+- replace the node to delete
+
+
+
+     */
+
+
+
     public Node insert(Node n, int x){
-	if (root = null)
-	    root = new Node(x);
-	else if (root.getData() > x)
-	    insert(root.getLeft(), x);
+	if (n = null)
+	    n = new Node(x);
+	else if (n.getData() > x)
+	    insert(n.getLeft(), x);
 	else 
-	    insert(root.getRight(), x);
+	    insert(n.getRight(), x);
     }
 
     public Node Search(int x){
@@ -29,12 +47,12 @@ public class BST{
     }
 
     public Node search2(Node c, int x){
-	if (node = null || node.getData() == x)
-	    return node;
+	if (c == null || c.getData() == x)
+	    return c;
 	else if (x < node.getData())
-	    return search2(node.left, x);
+	    return search2(c.getLeft(), x);
 	else
-	    return search2(node.right, x);
+	    return search2(c.getRight(), x);
     }
 
     public static void main(String[] args){
