d13151dd58a5ed559a65b50ee97fae52eff39322
Yoon Cho <yoon.cho@hotmail.com>
2014-05-13 00:38:51 -0400

    heapsort

--- cho_yoon/18-BinaryHeap/BinaryHeap.java
+++ cho_yoon/18-BinaryHeap/BinaryHeap.java
@@ -1,49 +1,72 @@
 public class BinaryHeap{
 
-    private Node root;
-
-    public BinaryHeap(){
-	root = null;
+    public void swap(int[] data,int pos1,int pos2){
+	int temp = data[pos1];
+	data[pos1] = data[pos2];
+	data[pos2] = temp;
     }
 
-    public Node insert(Node n, int x){
-	if (n == null)
-	    n = new Node(x);
-	else if (n.getData() > x)
-	    insert(n.getLeft(), x);
-	else 
-	    insert(n.getRight(), x);
-	return n;
-    }
+    public void heapSort(int[] data){
 
-    public Node Search(int x){
-	Node n = root;
-	while (n != null){
-	    if (n.getData() == x)
-		return n;
-	    else if (x < n.getData())
-		n = n.getLeft();
-	    else
-		n = n.getRight();
+	for (int x=0;x<data.length;x++)
+	    System.out.print(data[x]+ " ");
+	System.out.println("");
+
+	for (int y = 0; y < data.length; y++){
+	    int child = y;
+	    while (child > 0){
+		int parent = (child-1)/2;
+		if (data[child] > data[parent]){
+		    swap(data,child,parent);
+		    child = parent;
+		}
+		else
+		    child = 0;
+	    }
 	}
-	return null;
-    }
-
-
-    public void Sort(){}
-
-    public String transverse(){
-	Node n = root;
-	if (n == null){
-	    return;
-	}else{
-	    System.out.print(n.getData()+", ");
+	
+	int length = data.length;
+	int left = 0;
+	int right = 0;
+	int root = 0;
+	while (true){
+	    left = (root*2)+1;
+	    right = left-1;
+	    if (left >= length)
+		break;
+	    if (right >= length)
+		break;
+	    if (data[left] > data[root]){
+		if (data[left]>data[right]){
+		    swap(data,left,root);
+		    root = left;
+		}
+		else{
+		    swap(data,right,root);
+		    root = right;
+		}
+	    }else{
+		if (data[right]>data[root]){
+		    swap(data,right,root);
+		    root = right;
+		}else{
+		    break;
+		}
+	    }
 	}
+
+	for (int m=0;m<data.length;m++)
+	    System.out.print(data[m]+ " ");
+	System.out.println("");
+
     }
 
-    public String traverse(Node c){
-
+    public static void main(String[] args){
+	BinaryHeap b = new BinaryHeap();
+	int[] x = {26,9,3,2,8,459,2,3,9,4,2,67,9,3,3,8,9,4,7,7};
+	b.heapSort(x);
     }
+  
 
     //1. the root will always be greater than its children
     //2. the tree is as full as possible from left to right
